374c991d306422a36221618eec03b3b0987b893c
Keynes Kanno <keyneskanno@gmail.com>
2021-09-02 14:46:49 -0300

    Inserindo metodo saca

--- src/bytebank/Conta.java
+++ src/bytebank/Conta.java
@@ -6,9 +6,19 @@ public class Conta {
 	int agencia;
 	int numero;
 	String titular;
-	
+
 	public void deposita(double valor) {
-		this.saldo +=valor;
+		this.saldo += valor;
+	}
+
+	public boolean saca(double valor) {
+		if (this.saldo >= valor) {
+			this.saldo -=valor;
+			return true;
+		} else {
+			return false;
+		}
+
 	}
 
 }
--- src/bytebank/TestaMetodo.java
+++ src/bytebank/TestaMetodo.java
@@ -7,6 +7,15 @@ public class TestaMetodo {
 		Conta conta = new Conta();
 		conta.deposita(500);
 		System.out.println(conta.saldo);
+		
+		Conta contaDoPaulo = new Conta();
+		contaDoPaulo.saldo = 100;
+		contaDoPaulo.deposita(50);
+		System.out.println(contaDoPaulo.saldo);
+		
+		boolean conseguiuRetirar = contaDoPaulo.saca(20);
+		System.out.println("Saldo restante: " + contaDoPaulo.saldo);
+		System.out.println(conseguiuRetirar);
 	}
 
 }
